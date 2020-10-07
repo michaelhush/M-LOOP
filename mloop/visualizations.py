72e2b96b1e1e67c8d0404b7782870eb08fb290da
Zak V <zakven@mit.edu>
2020-10-07 19:19:43 -0400

    Corrected scalings in GaussianProcessVisualizer.return_cross_sections().
    The values in cross_parameter_arrays range from self.min_boundary to self.max_boundary which are already in real units, so it doesn't need to be scaled. The uncertainty arrays on the other hand do need to be unscaled

--- mloop/visualizations.py
+++ mloop/visualizations.py
@@ -846,17 +846,17 @@ class GaussianProcessVisualizer(mll.GaussianProcessLearner):
                 raise ValueError
         
         cross_parameter_arrays = [ np.linspace(min_p, max_p, points) for (min_p,max_p) in zip(self.min_boundary,self.max_boundary)]
-        cost_arrays = []
-        uncertainty_arrays = []
+        unscaled_cost_arrays = []
+        unscaled_uncertainty_arrays = []
         for ind in range(self.num_params):
             sample_parameters = np.array([cross_section_center for _ in range(points)])
             sample_parameters[:, ind] = cross_parameter_arrays[ind]
             (costs, uncers) = self.gaussian_process.predict(sample_parameters,return_std=True)
-            cost_arrays.append(costs)
-            uncertainty_arrays.append(uncers)
-        cross_parameter_arrays = np.array(cross_parameter_arrays)/self.cost_scaler.scale_
-        cost_arrays = self.cost_scaler.inverse_transform(np.array(cost_arrays))
-        uncertainty_arrays = np.array(uncertainty_arrays)
+            unscaled_cost_arrays.append(costs)
+            unscaled_uncertainty_arrays.append(uncers)
+        cross_parameter_arrays = np.array(cross_parameter_arrays)
+        cost_arrays = self.cost_scaler.inverse_transform(np.array(unscaled_cost_arrays))
+        uncertainty_arrays = np.array(unscaled_uncertainty_arrays) * self.cost_scaler.scale_
         return (cross_parameter_arrays,cost_arrays,uncertainty_arrays) 
     
     def create_visualizations(self,
