5a9fef8778c493dbcc01ce9238c71806b150259f
Ryan Morshead <ryan.morshead@gmail.com>
2024-08-13 15:42:34 -0600

    Require tensorflow>=2.16.1 and remove tensorflow-macos
    As of [Tensorflow 2.16.1](https://github.com/tensorflow/tensorflow/releases/tag/v2.16.1) `tensorflow-macos` has been deprecated.
    
    Releases of `tensorflow>=2.16.1` cannot be properly installed with `tensorflow-macos`.
    
    The solution is for `m-loop` to require `tensorflow>=2.16.1`

--- setup.py
+++ setup.py
@@ -35,8 +35,7 @@ def main():
             'matplotlib>=1.5',
             'pytest>=2.9',
             'scikit-learn>=0.18',
-            'tensorflow>=2.0.0; sys_platform != "darwin"',
-            'tensorflow-macos>=2.0.0; sys_platform == "darwin"',
+            'tensorflow>=2.16.1',
         ],
         tests_require=['pytest', 'setuptools>=26'],
         package_data = {
